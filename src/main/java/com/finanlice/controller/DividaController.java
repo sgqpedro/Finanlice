f22e08a3851c570c700516f3d014d5e6753eacf0
Pedro Henrique <pedro1cardoso19@gmail.com>
2026-08-26 01:01:20 -0300

    criando o DividaController

--- src/main/java/com/finanlice/controller/DividaController.java
+++ src/main/java/com/finanlice/controller/DividaController.java
@@ -1,4 +1,29 @@
 package com.finanlice.controller;
 
+
+import com.finanlice.model.DividaModel;
+import com.finanlice.service.DividaService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dividas")
 public class DividaController {
+
+
+    private final DividaService dividaService;
+
+    public DividaController(DividaService dividaService){
+        this.dividaService = dividaService;
+    }
+
+    @PostMapping
+    public DividaModel salvar(@RequestBody DividaModel divida ) {
+        return dividaService.salvar(divida);
+    }
+
+
+
 }
