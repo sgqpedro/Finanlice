1420982e0f662a8193ce98dbcbd4b32e36f0ea84
Pedro Henrique <pedro1cardoso19@gmail.com>
2026-09-13 21:01:51 -0300

    feat: add usuario Service

--- src/main/java/com/finanlice/service/UsuarioService.java
+++ src/main/java/com/finanlice/service/UsuarioService.java
@@ -8,6 +8,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsuarioService {
 
+    private final UsuarioRepository usuarioRepository;
 
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public UsuarioModel salvar(UsuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
 
 }
